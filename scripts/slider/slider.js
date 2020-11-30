1cd0543fc11ceb4eae33244512c3c63f7883fb18
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2020-11-30 21:14:18 +0300

    слайдер заполнен

---
+++ scripts/slider/slider.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.swiper-container');
+
+
+let mySwiper = new Swiper(slider, {
+
+  slidesPerView: 3,
+  spaceBetween: 10,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  loop: true,
+  // slidesPerGroup: 2,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+  preventClicks: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+})
