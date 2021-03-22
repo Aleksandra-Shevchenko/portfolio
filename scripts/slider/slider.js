70ff959258d25d4fa339023e94d5778dde66052d
Aleksandra-Shevchenko <shevdorosh@yandex.ru>
2021-03-22 20:33:51 +0300

    Кастомные названия стилей слайдера

--- scripts/slider/slider.js
+++ scripts/slider/slider.js
@@ -1,4 +1,4 @@
-const slider = document.querySelector('.swiper-container');
+const slider = document.querySelector('.slider__container');
 
 
 let mySwiper = new Swiper(slider, {
@@ -6,10 +6,28 @@ let mySwiper = new Swiper(slider, {
   slidesPerView: 3,
   spaceBetween: 50,
 
+  containerModifierClass: 'slider__container_',
+  wrapperClass: 'slider__wrapper',
+  slideClass: 'slider__slide',
+  slideActiveClass: 'slider__slide_active',
+  slideNextClass: 'slider__slide_next',
+  slidePrevClass: 'slider__slide_prev',
+  slideDuplicateActiveClass: 'slider__slide_duplicate_active',
+  slideDuplicateClass: 'slider__slide_duplicate',
+  slideDuplicateNextClass: 'slider__slide_duplicate_next',
+  slideDuplicatePrevClass: 'slider__slide_duplicate_prev',
+  slideVisibleClass: 'slider__slide_visible',
+  slideBlankClass: 'slider__slide_visible_invisible_blank',
+
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
+
+    // bulletActiveClass: 'slider__pagination_bullet_active',
+    // bulletClass: 'slider__pagination_bullet',
+    // modifierClass: 'slider-pagination_',
+    // clickableClass: 'slider__pagination_clickable',
   },
 
   loop: true,
